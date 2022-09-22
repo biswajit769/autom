e6cecb5bdaf158af7355b56ee0d5276be72eb125
biswajit769 <biswajit769@gmail.com>
2022-09-22 12:12:34 +0530

    bug fix

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ var storage = multer.diskStorage({
 })
 
 var upload = multer({ storage: storage })
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log('Server started on port',process.env.PORT);
 });
 
 app.get('/', function (req, res) {
