6869183abb768c8cf8d784e74449125cd1b367e3
biswajit769 <biswajit769@gmail.com>
2022-10-08 14:28:40 +0530

    changes

--- uploadinvoice.js
+++ uploadinvoice.js
@@ -16,66 +16,68 @@ async function uploadinvoice({applicationId, invoiceNo, invoiceDate, netpayble},
       ],
     });
     const page = await browser.newPage();
-    await page.goto(process.env.BASE_URL);
-    await page.waitFor(5000);
+    
+    await page.waitForTimeout(5000);
     await page.setViewport({ width: 1440, height: 788 });
-    await page.waitFor(5000);
+    await page.waitForTimeout(5000);
+    await page.goto(process.env.BASE_URL);
+    await page.waitForTimeout(5000);
     await page.waitForSelector(constants.USERNAME_SELECTOR);
     await page.click(constants.USERNAME_SELECTOR);
     await page.keyboard.type(userid);
-    await page.waitFor(5000);
+    await page.waitForTimeout(5000);
     await page.waitForSelector(constants.PASSWORD_SELECTOR);
     await page.click(constants.PASSWORD_SELECTOR);
     await page.keyboard.type(password);
-    await page.waitFor(5000);
+    await page.waitForTimeout(5000);
     await page.waitForSelector(constants.SUBMIT_BUTTON_SELECTOR);
     await page.click(constants.SUBMIT_BUTTON_SELECTOR);
     // await page.type("#mat-input-0", userid);
     // await page.type("#mat-input-1", password);
     // await page.click("#kt_login_signin_submit");
-    await page.waitFor(5000); // wait for 5 seconds
+    await page.waitForTimeout(5000); // wait for 5 seconds
     console.log("login completed",applicationId);
     //await browser.close();
 
     await page.waitForSelector('#kt_header_menu > ul > li:nth-child(3) > a',{timeout: 60000});
     await page.click('#kt_header_menu > ul > li:nth-child(3) > a');
-    await page.waitFor(5000); // wait for 5 seconds
+    await page.waitForTimeout(5000); // wait for 5 seconds
     console.log("header menu selection completed",applicationId);
 
     await page.waitForSelector('#kt_header_menu > ul > li.kt-menu__item.kt-menu__item--submenu.kt-menu__item--rel.ng-star-inserted.kt-menu__item--open-dropdown > div > ul > li > a',{timeout: 60000});
     await page.click('#kt_header_menu > ul > li.kt-menu__item.kt-menu__item--submenu.kt-menu__item--rel.ng-star-inserted.kt-menu__item--open-dropdown > div > ul > li > a');
-    await page.waitFor(5000);
+    await page.waitForTimeout(5000);
 
     await page.waitForSelector(constants.SEARCHBOX_SELECTOR,{timeout: 60000});
     await page.click(constants.SEARCHBOX_SELECTOR);
-    await page.waitFor(5000);
+    await page.waitForTimeout(5000);
 
     await page.type(constants.SEARCHBOX_SELECTOR, applicationId);
-    await page.waitFor(5000);
+    await page.waitForTimeout(5000);
     await page.keyboard.press('Enter');
-    await page.waitFor(5000);
+    await page.waitForTimeout(5000);
     console.log("search box enter done",applicationId);
 
     await page.waitForSelector('td[data-title="Application ID"] > span> a',{timeout: 120000});
     await page.click('td[data-title="Application ID"] > span> a');
-    await page.waitFor(5000);
+    await page.waitForTimeout(5000);
 
     await page.waitForSelector('#kt_content > div > kt-application-details > div.kt-portlet.kt-portlet--tabs > div > div > ul > li:nth-child(6) > a',{timeout: 60000});
     await page.click('#kt_content > div > kt-application-details > div.kt-portlet.kt-portlet--tabs > div > div > ul > li:nth-child(6) > a');
-    await page.waitFor(5000);
+    await page.waitForTimeout(5000);
 
     await page.waitForSelector('#kt_content > div > kt-application-details > div.kt-container-alt > kt-credit-line-withdrawal > div > div > div > div.kt-portlet__head > div.kt-portlet__head-toolbar > button',{timeout: 60000});
     await page.click('#kt_content > div > kt-application-details > div.kt-container-alt > kt-credit-line-withdrawal > div > div > div > div.kt-portlet__head > div.kt-portlet__head-toolbar > button');
-    await page.waitFor(8000);
+    await page.waitForTimeout(8000);
 
     await page.waitForSelector('mat-bottom-sheet-container > kt-withdrawal-request-form > form > button',{timeout: 60000});
     await page.click('mat-bottom-sheet-container > kt-withdrawal-request-form > form > button');
-    await page.waitFor(7000);
+    await page.waitForTimeout(7000);
 
     await page.waitForSelector('#kt_content > div > kt-application-details > div.kt-container-alt > kt-credit-line-withdrawal > div > div > div > div.kt-portlet__body > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr > td:nth-child(1) > input',{timeout: 60000});
     //await page.click('#kt_content > div > kt-application-details > div.kt-container-alt > kt-credit-line-withdrawal > div > div > div > div.kt-portlet__body > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr > td:nth-child(1) > input');
     await page.type('#kt_content > div > kt-application-details > div.kt-container-alt > kt-credit-line-withdrawal > div > div > div > div.kt-portlet__body > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr > td:nth-child(1) > input', netpayble);
-    await page.waitFor(5000);
+    await page.waitForTimeout(5000);
     console.log("net payble entry done");
     
     //invoice date
@@ -89,30 +91,30 @@ async function uploadinvoice({applicationId, invoiceNo, invoiceDate, netpayble},
     await page.type('#kt_content > div > kt-application-details > div.kt-container-alt > kt-credit-line-withdrawal > div > div > div > div.kt-portlet__body > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr > td:nth-child(3) > div > input', invoiceDate);
     
     //await page.keyboard.type(invoiceDate);
-    await page.waitFor(5000);
+    await page.waitForTimeout(5000);
     console.log("date entry done");
 
     await page.waitForSelector('#kt_content > div > kt-application-details > div.kt-container-alt > kt-credit-line-withdrawal > div > div > div > div.kt-portlet__body > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr > td:nth-child(3) > div > div > button',{timeout: 60000});
     await page.click('#kt_content > div > kt-application-details > div.kt-container-alt > kt-credit-line-withdrawal > div > div > div > div.kt-portlet__body > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr > td:nth-child(3) > div > div > button');
-    await page.waitFor(1000);
+    await page.waitForTimeout(1000);
 
     await page.waitForSelector('#kt_content > div > kt-application-details > div.kt-container-alt > kt-credit-line-withdrawal > div > div > div > div.kt-portlet__body > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr > td:nth-child(4) > input',{timeout: 60000});
     //await page.click('#kt_content > div > kt-application-details > div.kt-container-alt > kt-credit-line-withdrawal > div > div > div > div.kt-portlet__body > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr > td:nth-child(1) > input');
     await page.type('#kt_content > div > kt-application-details > div.kt-container-alt > kt-credit-line-withdrawal > div > div > div > div.kt-portlet__body > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr > td:nth-child(4) > input', invoiceNo);
-    await page.waitFor(5000);
+    await page.waitForTimeout(5000);
 
     await page.waitForSelector('#invoice_file',{timeout: 60000});
-    await page.waitFor(1000);
+    await page.waitForTimeout(1000);
 
     const inputUploadHandle = await page.$('#invoice_file');
     let fileToUpload = storagefolder+'/'+fileName;
     inputUploadHandle.uploadFile(fileToUpload);
-    await page.waitFor(5000);
+    await page.waitForTimeout(5000);
     console.log("file upload done",applicationId);
 
     await page.waitForSelector('#kt_content > div > kt-application-details > div.kt-container-alt > kt-credit-line-withdrawal > div > div > div > div.kt-portlet__body > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr > td.text-right > button',{timeout: 60000});
     await page.click('#kt_content > div > kt-application-details > div.kt-container-alt > kt-credit-line-withdrawal > div > div > div > div.kt-portlet__body > table > tbody > tr:nth-child(2) > td:nth-child(2) > form > table > tbody > tr > td.text-right > button');
-    await page.waitFor(5000);
+    await page.waitForTimeout(5000);
 
     await page.waitForSelector('#swal2-title',{timeout: 100000});
     let labelelement = await page.$('#swal2-title');
