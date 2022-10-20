2f84f1c48afc44ac6b2920a44bc589cc1691a177
biswajit769 <biswajit769@gmail.com>
2022-10-20 11:03:03 +0530

    changes

--- createInvoice.js
+++ createInvoice.js
@@ -17,10 +17,14 @@ const createInvoice = async (invdetail, {userid, password}) => {
     });
     const page = await browser.newPage();
     await page.goto(process.env.BASE_URL);
-    await page.waitFor(500);
-    await page.waitForSelector(constants.USERNAME_SELECTOR);
-    await page.click(constants.USERNAME_SELECTOR);
-    await page.keyboard.type(`${userid}`);
+    // await page.waitFor(500);
+    // await page.waitForSelector(constants.USERNAME_SELECTOR);
+    // await page.click(constants.USERNAME_SELECTOR);
+    // await page.keyboard.type(`${userid}`);
+    
+
+    // Type into search box.
+    await page.type(constants.USERNAME_SELECTOR, userid);
     console.log("username entered");
     await browser.close();
     return 'success'; 
